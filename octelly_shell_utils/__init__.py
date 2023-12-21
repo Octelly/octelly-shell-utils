4d6297bc32f83d5cf831e8d5d63484d4e8b6e520
Octelly <eli@stefek.cz>
2023-12-21 15:43:11 +0100

    root callback is still a placeholder

--- octelly_shell_utils/__init__.py
+++ octelly_shell_utils/__init__.py
@@ -8,12 +8,14 @@ for name, entry in utils.items():
     app.add_typer(entry, name=name)
 
 
-@app.callback(invoke_without_command=True)
+@app.callback(
+    # invoke_without_command=True
+)
 def main(ctx: typer.Context):
     """
     Octelly's shell utilities
     """
-    if ctx.invoked_subcommand is None:
-        print(":)")
-        for name, entry in utils.items():
-            print(entry.info.help)
+    # if ctx.invoked_subcommand is None:
+    #    print(":)")
+    #    for name, entry in utils.items():
+    #        print(entry.info.help)
